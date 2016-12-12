a7e08336f9f4d78c106ecb6d136621c8de41cbee
FabianoTM <fabiano.tiengo@gmail.com>
2016-12-12 09:53:59 -0200

    Create status.php

---
+++ status.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+<body>
+  <h1>STATUS: ok</h1>
+	<div id="chartContainer" style="height:400px; width:100%;"></div>
+</body>
